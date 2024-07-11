a9bbc000883acfd798f7c192fb67a394039150b5
Carnegie2610 <ronsard44djoumessi@gmail.com>
2024-07-11 16:26:13 +0100

    second commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./experience.css";
-import ListExperiences from "./components/listExperiences";
+
 
 export const Experience = () => {
   return (
@@ -8,9 +8,7 @@ export const Experience = () => {
       <h5>What skills i have</h5>
       <h2>My Experience</h2>
       <div className="container experience__container">
-        <ListExperiences 
-        description = "html"
-        />
+      
       </div>
     </section>
   );
